503c25f0f189566c9af1d5c445c973132d460f5e
sean <sennis3@uic.edu>
2020-09-12 18:36:48 -0500

    Date and Time work

---
+++ Assets/DateScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DateScript : MonoBehaviour
+{
+    public GameObject dateTextObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    InvokeRepeating("UpdateDate", 0f, 30f);   
+    }
+
+    // Update is called once per frame
+    void UpdateDate()
+    {
+    dateTextObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString("d");
+       
+    }
+}
